9e2f0f86432ad263226dfca78979822bc3365bf4
Leul Sileshi <106759704+Lulusha-XT@users.noreply.github.com>
2023-04-07 22:25:43 +0300

    befor tedy pushed

--- Frontend/lib/src/features/core/pages/profile/profile_screen.dart
+++ Frontend/lib/src/features/core/pages/profile/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/src/common_widgets/slider_widget/slider.dart';
 import 'package:flutter_application_1/src/constants/colors.dart';
 import 'package:flutter_application_1/src/constants/image_strings.dart';
 import 'package:flutter_application_1/src/constants/sizes.dart';
@@ -24,173 +25,225 @@ class ProfileScreen extends StatelessWidget {
           child: Container(
         child: SizedBox(
           height: MediaQuery.of(context).size.height,
-          child: Column(children: [
-            Container(
-              padding: const EdgeInsets.all(cDefaultSize),
-              decoration: const BoxDecoration(
-                color: cPrimaryColor,
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(cDefaultSize),
+                decoration: const BoxDecoration(
+                  color: cPrimaryColor,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Stack(
+                          children: [
+                            SizedBox(
+                              width: 120,
+                              height: 120,
+                              child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(100),
+                                  child: const Image(
+                                      image: AssetImage(cProfileImage))),
+                            ),
+                            Positioned(
+                              bottom: 0,
+                              right: 0,
+                              child: Container(
+                                width: 35,
+                                height: 35,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(100),
+                                  color: cPrimaryColor,
+                                ),
+                                child: IconButton(
+                                  icon: const Icon(
+                                    LineAwesomeIcons.camera,
+                                    color: Colors.white,
+                                    size: 20,
+                                  ),
+                                  onPressed: () async {
+                                    //  pick imaGe
+                                    // install image_picker
+                                    // import the corresponding library
+                                    ImagePicker imagePicker = ImagePicker();
+                                    XFile? file = await imagePicker.pickImage(
+                                        source: ImageSource.camera);
+                                    print("${file?.path}");
+                                    if (file == null) return;
+
+                                    String uniqueFileName = DateTime.now()
+                                        .millisecondsSinceEpoch
+                                        .toString();
+                                  },
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: const [
+                            Text(
+                              "Leul sileshi",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Text("A+", style: TextStyle(color: Colors.white))
+                          ],
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 70,
+                      height: 70,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: cPrimaryColor,
+                      ),
+                      child: const Icon(
+                        LineAwesomeIcons.alternate_pencil,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30))),
+                  child: Column(
                     children: [
-                      Stack(
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           SizedBox(
-                            width: 120,
-                            height: 120,
-                            child: ClipRRect(
-                                borderRadius: BorderRadius.circular(100),
-                                child: const Image(
-                                    image: AssetImage(cProfileImage))),
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            right: 0,
-                            child: Container(
-                              width: 35,
-                              height: 35,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(100),
-                                color: cPrimaryColor,
+                            width: 150,
+                            height: 80,
+                            child: Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
                               ),
-                              child: IconButton(
-                                icon: const Icon(
-                                  LineAwesomeIcons.camera,
-                                  color: Colors.white,
-                                  size: 20,
+                              elevation: 5,
+                              child: Padding(
+                                padding: const EdgeInsets.all(16),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const [
+                                    Text(
+                                      'Birth Date',
+                                      style: TextStyle(
+                                        color: Colors.red,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    SizedBox(height: 8),
+                                    Text(
+                                      '07/04/2023',
+                                      style: TextStyle(
+                                        color: Colors.red,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                                onPressed: () async {
-                                  //  pick imaGe
-                                  // install image_picker
-                                  // import the corresponding library
-                                  ImagePicker imagePicker = ImagePicker();
-                                  XFile? file = await imagePicker.pickImage(
-                                      source: ImageSource.camera);
-                                  print("${file?.path}");
-                                  if (file == null) return;
-
-                                  String uniqueFileName = DateTime.now()
-                                      .millisecondsSinceEpoch
-                                      .toString();
-                                },
                               ),
                             ),
-                          )
+                          ),
+                          SizedBox(
+                            width: 150,
+                            height: 80,
+                            child: Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              elevation: 5,
+                              child: Padding(
+                                padding: const EdgeInsets.all(16),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const [
+                                    Text(
+                                      'Blood Type',
+                                      style: TextStyle(
+                                        color: Colors.red,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    SizedBox(height: 8),
+                                    Text(
+                                      'AB+',
+                                      style: TextStyle(
+                                        color: Colors.red,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
                         ],
                       ),
-                      Column(
-                        children: const [
-                          Text(
-                            "Leul sileshi",
-                            style: TextStyle(color: Colors.white),
+                      SizedBox(
+                        width: 350,
+                        height: 80,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
                           ),
-                          Text("A+", style: TextStyle(color: Colors.white))
-                        ],
+                          elevation: 5,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: const [
+                                    Text(
+                                      'Height:',
+                                      style: TextStyle(
+                                        color: Colors.red,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 12,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    SizedBox(width: 10),
+                                    Text(
+                                      'cm',
+                                      style: TextStyle(
+                                        color: Colors.red,
+                                        fontSize: 12,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ],
+                                ),
+                                SliderFb1(
+                                  max: 100,
+                                  min: 0,
+                                  onChange: (double) {},
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
                       ),
                     ],
                   ),
-                  Container(
-                    width: 70,
-                    height: 70,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(100),
-                      color: cPrimaryColor,
-                    ),
-                    child: const Icon(
-                      LineAwesomeIcons.alternate_pencil,
-                      color: Colors.white,
-                      size: 20,
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30))),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        width: 150,
-                        height: 80,
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          elevation: 5,
-                          child: Padding(
-                            padding: const EdgeInsets.all(16),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: const [
-                                Text(
-                                  'Birth Date',
-                                  style: TextStyle(
-                                    color: Colors.red,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 15,
-                                  ),
-                                ),
-                                SizedBox(height: 8),
-                                Text(
-                                  '07/04/2023',
-                                  style: TextStyle(
-                                    color: Colors.red,
-                                    fontSize: 12,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 150,
-                        height: 80,
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          elevation: 5,
-                          child: Padding(
-                            padding: const EdgeInsets.all(16),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: const [
-                                Text(
-                                  'Blood Type',
-                                  style: TextStyle(
-                                    color: Colors.red,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 15,
-                                  ),
-                                ),
-                                SizedBox(height: 8),
-                                Text(
-                                  'AB+',
-                                  style: TextStyle(
-                                    color: Colors.red,
-                                    fontSize: 12,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ]),
-              ),
-            )
-          ]),
+            ],
+          ),
         ),
       )),
     );
