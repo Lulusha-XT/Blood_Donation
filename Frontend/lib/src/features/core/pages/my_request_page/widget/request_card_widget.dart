361ced4172ef8c24441a451ab0f58116c0c54585
Leul Sileshi <106759704+Lulusha-XT@users.noreply.github.com>
2023-04-28 13:08:15 +0300

    card

--- Frontend/lib/src/features/core/pages/my_request_page/widget/request_card_widget.dart
+++ Frontend/lib/src/features/core/pages/my_request_page/widget/request_card_widget.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/src/constants/colors.dart';
 
 class RequestCardWidget extends StatelessWidget {
-  const RequestCardWidget({super.key});
+  const RequestCardWidget({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -10,17 +12,41 @@ class RequestCardWidget extends StatelessWidget {
       padding: const EdgeInsets.all(8),
       child: Column(
         children: [
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 8,
-            ),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: cPrimaryColor,
+          const SizedBox(height: 16),
+          SizedBox(
+            width: 600,
+            height: 160,
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  side: const BorderSide(color: cPrimaryColor)),
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Blood Request',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'O+ Blood is needed urgently. Please contact the below number if you can donate.',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Contact: +123 456 7890',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
               ),
-              borderRadius: BorderRadius.circular(15),
             ),
-            height: 100,
           ),
         ],
       ),
